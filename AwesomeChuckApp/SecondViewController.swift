6a4298b31bbdff502e9e5995a7172ccb3cb70d82
Denis Rakitin <piggest@mail.ru>
2019-05-28 22:28:33 +0300

    Initial Commit

---
+++ AwesomeChuckApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AwesomeChuckApp
+//
+//  Created by Denis Rakitin on 2019-05-28.
+//  Copyright © 2019 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
